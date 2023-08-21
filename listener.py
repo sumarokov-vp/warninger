38f6d06ebf3044bcb0d373f3d209f5fdd28cc61d
sumarokov-vp <sumarokov.vp@gmail.com>
2023-08-21 20:14:32 +0600

    fix

--- listener.py
+++ listener.py
@@ -20,27 +20,26 @@ def everythingisok():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
+        with Session(engine) as session:
+            warning = session.scalar(
+                select(Warning)
+                .where(Warning.id == json['warning_id'])
+            )
+            if warning:
+                warning.last_success = datetime.now() # type: ignore
+                session.commit()
+                message = {'message': f'Warning_id {warning.id} is OK'}
+                response = make_response(jsonify(message), 200)
+            else:
+                warning_id = json['warning_id']
+                message = {'message': f'Warning_id {warning_id} is not found!'}
+                response = make_response(jsonify(message), 406)
     else:
         message = {'message': f'Content-Type not supported!'}
         response = make_response(jsonify(message), 415)
-        return response
-    print (json)
-    with Session(engine) as session:
-        warning = session.scalar(
-            select(Warning)
-            .where(Warning.id == json['warning_id'])
-        )
-        if warning:
-            warning.last_success = datetime.now() # type: ignore
-            session.commit()
-            message = {'message': f'Warning_id {warning.id} is OK'}
-            response = make_response(jsonify(message), 200)
-            return response
-        else:
-            warning_id = json['warning_id']
-            message = {'message': f'Warning_id {warning_id} is not found!'}
-            response = make_response(jsonify(message), 404)
-            return response
+
+    print(response)
+    return response
 
 @app.route('/everythingisok', methods=['GET', 'PUT']) # type: ignore
 def everythingisok_wrong_method():
@@ -51,7 +50,7 @@ def everythingisok_wrong_method():
 # any other request must return 404 and print request data
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE']) # type: ignore
 def catch_all(path):
-    message = {'message': f'Not found!'}
+    message = {'message': f'Endpoint not found!'}
     response = make_response(jsonify(message), 404)
     print(request)
     return response
