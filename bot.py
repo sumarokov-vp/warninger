4e091b097f7396fabc8200f1c5c72967bc3ff8b4
sumarokov-vp <sumarokov.vp@gmail.com>
2023-08-25 19:42:22 +0600

    bot token none

--- bot.py
+++ bot.py
@@ -6,5 +6,8 @@ if not bot_token:
     raise Exception("BOT_TOKEN env variable is not set")
 if bot_token =='':
     raise Exception("BOT_TOKEN env variable is empty")
+if bot_token == 'None':
+    raise Exception("BOT_TOKEN env variable is None")
+
 print(f"Bot token: {bot_token}")
 bot = TeleBot(bot_token)
