7718d117ace3fa5317dbb84468dabb593ce97656
sumarokov-vp <sumarokov.vp@gmail.com>
2023-08-25 19:39:47 +0600

    bot token var is empty

--- bot.py
+++ bot.py
@@ -2,4 +2,8 @@ from telebot import TeleBot
 from os import environ
 
 bot_token = str(environ.get('BOT_TOKEN'))
+if not bot_token:
+    raise Exception("BOT_TOKEN env variable is not set")
+if bot_token =='':
+    raise Exception("BOT_TOKEN env variable is empty")
 bot = TeleBot(bot_token)
