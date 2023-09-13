cb03ce2192519625d62358e69c89bc61cbf14432
sumarokov-vp <sumarokov.vp@gmail.com>
2023-09-13 11:24:35 +0600

    report add header

--- reporter.py
+++ reporter.py
@@ -1,4 +1,3 @@
-from logging import warn
 from time import sleep
 from sqlalchemy import select
 from datetime import datetime, timedelta
@@ -6,39 +5,35 @@ from datetime import datetime, timedelta
 from db_engine import engine
 from sqlalchemy.orm import Session
 from models import Warning
-from models import Recipient
-from bot import bot
-from telebot import apihelper
 from settings import get_setting
 
-def process_warning(warning: Warning, session: Session) -> int:
 
+def process_warning(warning: Warning, session: Session) -> int:
     # Get datetime of start notify based on last_success
     print(f"\nWarning name: {warning.name}")
     print(f"Now datetime: {datetime.now()}")
     print(f"Last success: {warning.last_success}")
 
-    if not warning.last_success: # type: ignore
+    if not warning.last_success:  # type: ignore
         return -1
-    notification_timeout: datetime = warning.last_success + timedelta( # type: ignore
-        days=warning.wait_days, # type: ignore
-        hours=warning.wait_hours, # type: ignore
-        minutes=warning.wait_minutes, # type: ignore
-        seconds=warning.wait_seconds, # type: ignore
+    notification_timeout: datetime = warning.last_success + timedelta(  # type: ignore
+        days=warning.wait_days,  # type: ignore
+        hours=warning.wait_hours,  # type: ignore
+        minutes=warning.wait_minutes,  # type: ignore
+        seconds=warning.wait_seconds,  # type: ignore
     )
     print(f"Notification start time: {notification_timeout}")
 
-
     # Get datetime of next notification based on last_notification
     next_notification: datetime
-    if not warning.last_notification: # type: ignore
-        next_notification = warning.last_success # type: ignore
+    if not warning.last_notification:  # type: ignore
+        next_notification = warning.last_success  # type: ignore
     else:
-        next_notification = warning.last_notification + timedelta( # type: ignore
-            days= warning.repeat_days or 0, # type: ignore
-            hours= warning.repeat_hours or 0, # type: ignore
-            minutes= warning.repeat_minutes or 0, # type: ignore
-            seconds= warning.repeat_seconds or 0, # type: ignore
+        next_notification = warning.last_notification + timedelta(  # type: ignore
+            days=warning.repeat_days or 0,  # type: ignore
+            hours=warning.repeat_hours or 0,  # type: ignore
+            minutes=warning.repeat_minutes or 0,  # type: ignore
+            seconds=warning.repeat_seconds or 0,  # type: ignore
         )
 
     # Notify if next notification is in the past
@@ -54,11 +49,11 @@ Last success signal: {warning.last_success}
 </code>
 """
             warning.all_recipients_mailing(
-                session= session,
-                message= message,
+                session=session,
+                message=message,
             )
-            warning.last_notification = datetime.now() # type: ignore
-            warning.status_id = 2 # type: ignore
+            warning.last_notification = datetime.now()  # type: ignore
+            warning.status_id = 2  # type: ignore
             session.commit()
             return 0
         else:
@@ -66,26 +61,29 @@ Last success signal: {warning.last_success}
             print(f"Next_notification: {next_notification}")
             return 1
     else:
-        warning.last_notification = None # type: ignore
+        warning.last_notification = None  # type: ignore
         session.commit()
-        print(f"Warning status is OK")
+        print("Warning status is OK")
         return 2
+
 
 if __name__ == "__main__":
     while True:
-        print(f"Start loop -- {datetime.now()}")
-        with Session(engine, expire_on_commit= False) as session:
+        with Session(engine, expire_on_commit=False) as session:
             warnings = session.scalars(
                 select(Warning)
-                .where(Warning.enabled == True) # type: ignore
-                .where(Warning.last_success != None) # type: ignore
+                .where(Warning.enabled == True)  # noqa E712
+                .where(Warning.last_success != None)  # noqa E711
             )
             for warning in warnings:
                 # try:
-                process_warning(warning= warning, session= session)
+                process_warning(warning=warning, session=session)
                 # except apihelper.ApiTelegramException as e:
                 #     print(f"Telegram API error: {e}")
                 #     session.rollback()
                 #     continue
-        sleep_time = int(get_setting('reporter_sleep_time_sec'))
+        sleep_time = int(get_setting("reporter_sleep_time_sec"))
+        print(
+            f"Sleep for {sleep_time} seconds \n ----------------------------------------"  # noqa E501
+        )  # noqa E501
         sleep(sleep_time)
